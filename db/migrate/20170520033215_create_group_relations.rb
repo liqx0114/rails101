c0ced0a5eab8dc7e43b0188fa141f87d95304d24
liqx0114 <liqxxuexi@gmail.com>
2017-05-20 13:24:13 +0800

    implement group relation

---
+++ db/migrate/20170520033215_create_group_relations.rb
@@ -0,0 +1,10 @@
+class CreateGroupRelations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :group_relations do |t|
+      t.integer :group_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
